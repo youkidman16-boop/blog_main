72216d689d45961a784a35eb35949d2433c2267b
artaustin09 <a.r.t.austin09@gmail.com>
2026-03-17 22:40:19 -0700

    creacion de modelo de la aplicacion blog

---
+++ blog/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_lenght = 225)
+    
+    class Meta:
+        ordering = ('title')
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+class Post (models.Model):
+
+    ACTIVE= 'active'
+    DRAFT= 'draft'
+
+    CHOICES_STATUS= {
+        (ACTIVE,'Active'),
+        (DRAFT,'Draft')
+    }        
+
+    category = models.ForeignKey(Category, related_name='post', on_delete= models.CASCADE)
+    title = models.CharField(max_length=225)
+    intro = models.TextField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add= True)
+    Status = models.CharField(max_length=10, choices= CHOICES_STATUS, default=ACTIVE)
+    image = models.ImageField(upload_to='upload/',blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post= models.ForeignKey(Post,related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=225)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateField(auto_now_add= True )
+
+    def __str__(self):
+        return self.name
